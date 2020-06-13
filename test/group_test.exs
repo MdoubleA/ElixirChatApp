89809edd22d1de2b87dc197dff79326a410ad5a9
MdoubleA <needanothermichael@gmail.com>
2020-06-12 23:42:01 -0700

    Clean testing suit. Begin transition to Elixir persistant data storage.

--- test/group_test.exs
+++ test/group_test.exs
@@ -13,19 +13,34 @@ defmodule TestGroupModule do
 		group = Group.new()
 
 		assert Group.new() == %Group{group_name: nil, num_people: 0, people: %{}}
+
+		# Test that add member works. Create a group so we can see the state changes stack.
 		assert Group.add_member(group, eliot1) == %Group{group_name: nil, num_people: 1, people: %{uniquename => eliot1}}
 		group = Group.add_member(group, eliot1)
+
+		# Test add name works.
 		assert Group.add_name(group, "TheCrew") == %Group{group_name: "TheCrew", num_people: 1, people: %{uniquename => eliot1}}
+		group = Group.add_name(group, "TheCrew")
+
+		# Test update member by stack interests.
 		group = Group.update_member(group, uniquename, :interests, interest2)
-		assert Group.add_name(group, "TheCrew") == %Group{group_name: "TheCrew", num_people: 1, people:
+		assert group == %Group{group_name: "TheCrew", num_people: 1, people:
 			%{uniquename =>
 			%Person{uniquename: uniquename, name: name, birthdate: ~D[1983-07-12], interests: [interest2, interest1]}}}
+
+		# Test the ability to get a specific member.
 		assert Group.get_member(group, uniquename) ==
 				%Person{uniquename: uniquename, name: name, birthdate: ~D[1983-07-12], interests: [interest2, interest1]}
-		group = Group.add_name(group, "TheCrew")
+
+		# Test removing member.
 		assert Group.del_member(group, uniquename) == %Group{group_name: "TheCrew", num_people: 0, people: %{}}
+
+		# Visually validate data format.
 		test_group = Group.from_file!("C:\\Users\\Michael\\ElixirProjects\\socialnetwork\\lib\\SomePeople.txt")
-		#IO.inspect(test_group)
+		IO.inspect(test_group)
+
+		# Test Enumerable protocol was properly implemented
+		# Test that we can turn a list [{uniquename, person}] back to a group.
 		assert Group.to_group(Enum.reverse(test_group), test_group) == test_group
 	end
 end # End TestGroupModule
