cbbbbb6227843b3a1ff6eb3c483346ac47bb3a56
MdoubleA <needanothermichael@gmail.com>
2020-07-14 22:51:36 -0700

    Clear repository for web restructure.

--- lib/system/application.ex
+++
@@ -1,7 +0,0 @@
-defmodule Socialnetwork.Application do
-	use Application
-
-	def start(_, _) do
-		Socialnetwork.MessageBoard.System.start_link()
-	end
-end  # end module definition
--- test/socialnetwork_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SocialnetworkTest do
-  use ExUnit.Case
-  doctest Socialnetwork
-
-  test "greets the world" do
-    assert Socialnetwork.hello() == :world
-  end
-end
